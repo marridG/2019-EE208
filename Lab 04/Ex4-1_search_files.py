6659a1d7e270f018bc4e4bb770840a92fb44eda8
marridG <1687450542@qq.com>
2019-10-07 10:54:49 +0800

    lab 04 drafts: almost done! [done]    chinese searcher

--- Lab 04/Ex4-1_search_files.py
+++ Lab 04/Ex4-1_search_files.py
@@ -12,6 +12,8 @@ from org.apache.lucene.store import SimpleFSDirectory
 from org.apache.lucene.search import IndexSearcher
 from org.apache.lucene.util import Version
 
+import jieba
+
 """
 This script is loosely based on the Lucene (java implementation) demo class 
 org.apache.lucene.demo.SearchFiles.  It will prompt for a search query, then it
@@ -21,12 +23,18 @@ search query entered against the 'contents' field.  It will then display the
 search.close() is currently commented out because it causes a stack overflow in
 some cases.
 """
+
+
 def run(searcher, analyzer):
     while True:
         print
         print "Hit enter with no input to quit."
         command = raw_input("Query:")
-        command = unicode(command, 'GBK')
+        #### print command
+        #### print type(command)
+        #### command = unicode(command, 'GBK')
+        #### print command
+        command = " ".join(jieba.cut(command))
         if command == '':
             return
 
@@ -46,7 +54,7 @@ if __name__ == '__main__':
     STORE_DIR = "index"
     lucene.initVM(vmargs=['-Djava.awt.headless=true'])
     print 'lucene', lucene.VERSION
-    #base_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+    # base_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
     directory = SimpleFSDirectory(File(STORE_DIR))
     searcher = IndexSearcher(DirectoryReader.open(directory))
     analyzer = StandardAnalyzer(Version.LUCENE_CURRENT)
